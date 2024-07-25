c10c7543778cb004714a425325f6ec4afea55988
Dominic Canare <dom@dominiccanare.com>
2024-07-25 16:54:46 -0400

    Rename base stream

--- src/pupil_labs/neon_recording/stream/av_stream/audio_stream.py
+++ src/pupil_labs/neon_recording/stream/av_stream/audio_stream.py
@@ -1,9 +1,9 @@
 from ... import structlog
-from .base_stream import BaseStream
+from .base_av_stream import BaseAVStream
 
 log = structlog.get_logger(__name__)
 
 
-class AudioStream(BaseStream):
+class AudioStream(BaseAVStream):
     def __init__(self, recording):
         super().__init__("audio", "Neon Scene Camera v1", recording, "audio")
