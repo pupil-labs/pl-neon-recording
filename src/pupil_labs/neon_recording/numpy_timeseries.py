937aa2bcdea6e425a8cc1eb19fdf7498531714d3
Marc Tonsen <mar@pupil-labs.com>
2024-11-03 15:33:07 +0100

    Added sample method

--- src/pupil_labs/neon_recording/numpy_timeseries.py
+++ src/pupil_labs/neon_recording/numpy_timeseries.py
@@ -2,15 +2,16 @@ from typing import Iterator, Optional, overload
 
 import numpy as np
 import numpy.typing as npt
+import pandas as pd
 
-from pupil_labs.matching import Timeseries
+from pupil_labs.matching import Matcher, MatchingMethod, TimesArray, Timeseries
 from pupil_labs.video.array_like import ArrayLike
 
 
 class NumpyTimeseries(Timeseries):
     def __init__(
         self,
-        timestamps: npt.NDArray[np.int32 | np.float32],
+        timestamps: TimesArray,
         data: Optional[npt.NDArray] = None,
     ):
         self.timestamps = timestamps
@@ -33,3 +34,14 @@ class NumpyTimeseries(Timeseries):
     def __iter__(self) -> Iterator[int]:
         for i in range(len(self)):
             yield self[i]
+
+    def sample(
+        self, target_ts: TimesArray, method: MatchingMethod = MatchingMethod.NEAREST
+    ) -> ArrayLike:
+        return Matcher(target_ts, self, method)
+
+    @staticmethod
+    def from_dataframe(df: pd.DataFrame, ts_column: str) -> "NumpyTimeseries":
+        time: TimesArray = df[ts_column].values.astype(np.float64)
+        data = df.drop(columns=[ts_column]).values
+        return NumpyTimeseries(time, data)
