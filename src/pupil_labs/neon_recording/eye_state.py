b66249cdcfa826e8de32e3012677f984e69a671b
Marc Tonsen <mar@pupil-labs.com>
2024-11-10 13:27:26 +0100

    Use Record datatypes for iteration

--- src/pupil_labs/neon_recording/eye_state.py
+++ src/pupil_labs/neon_recording/eye_state.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from typing import NamedTuple
 
 import numpy as np
 import numpy.typing as npt
@@ -9,81 +10,63 @@ from pupil_labs.neon_recording.utils import (
 )
 
 
-class EyeState(dict):
-    timestamps: npt.NDArray[np.int64]
-    pupil_diameter_left: npt.NDArray[np.float64]
-    eyeball_center_left_x: npt.NDArray[np.float64]
-    eyeball_center_left_y: npt.NDArray[np.float64]
-    eyeball_center_left_z: npt.NDArray[np.float64]
-    optical_axis_left_x: npt.NDArray[np.float64]
-    optical_axis_left_y: npt.NDArray[np.float64]
-    optical_axis_left_z: npt.NDArray[np.float64]
-    pupil_diameter_right: npt.NDArray[np.float64]
-    eyeball_center_right_x: npt.NDArray[np.float64]
-    eyeball_center_right_y: npt.NDArray[np.float64]
-    eyeball_center_right_z: npt.NDArray[np.float64]
-    optical_axis_right_x: npt.NDArray[np.float64]
-    optical_axis_right_y: npt.NDArray[np.float64]
-    optical_axis_right_z: npt.NDArray[np.float64]
+class EyeStateRecord(NamedTuple):
+    ts: int
+    pupil_diameter_left: float
+    eyeball_center_left_x: float
+    eyeball_center_left_y: float
+    eyeball_center_left_z: float
+    optical_axis_left_x: float
+    optical_axis_left_y: float
+    optical_axis_left_z: float
+    pupil_diameter_right: float
+    eyeball_center_right_x: float
+    eyeball_center_right_y: float
+    eyeball_center_right_z: float
+    optical_axis_right_x: float
+    optical_axis_right_y: float
+    optical_axis_right_z: float
 
+
+class EyeState:
     def __init__(self, rec_dir: Path):
         eye_state_files = find_sorted_multipart_files(rec_dir, "eye_state")
         eye_state_data, time_data = load_multipart_data_time_pairs(
             eye_state_files, "<f4", 2
         )
-        data = eye_state_data.reshape(-1, 14)
+        self._time_data = time_data
+        self._data = eye_state_data.reshape(-1, 14)
 
-        self["timestamps"] = time_data
-        self["pupil_diameter_left"] = data[:, 0]
-        self["eyeball_center_left_x"] = data[:, 1]
-        self["eyeball_center_left_y"] = data[:, 2]
-        self["eyeball_center_left_z"] = data[:, 3]
-        self["optical_axis_left_x"] = data[:, 4]
-        self["optical_axis_left_y"] = data[:, 5]
-        self["optical_axis_left_z"] = data[:, 6]
-        self["pupil_diameter_right"] = data[:, 7]
-        self["eyeball_center_right_x"] = data[:, 8]
-        self["eyeball_center_right_y"] = data[:, 9]
-        self["eyeball_center_right_z"] = data[:, 10]
-        self["optical_axis_right_x"] = data[:, 11]
-        self["optical_axis_right_y"] = data[:, 12]
-        self["optical_axis_right_z"] = data[:, 13]
+    @property
+    def timestamps(self) -> npt.NDArray[np.int64]:
+        return self._time_data
+
+    ts = timestamps
 
     @property
     def pupil_diameters(self) -> npt.NDArray[np.float64]:
-        return np.column_stack([self.pupil_diameter_left, self.pupil_diameter_right])
+        return self._data[:, [0, 7]]
 
     @property
     def eye_ball_center_left(self) -> npt.NDArray[np.float64]:
-        return np.column_stack([
-            self.eyeball_center_left_x,
-            self.eyeball_center_left_y,
-            self.eyeball_center_left_z,
-        ])
+        return self._data[:, [1, 2, 3]]
 
     @property
     def eye_ball_center_right(self) -> npt.NDArray[np.float64]:
-        return np.column_stack([
-            self.eyeball_center_right_x,
-            self.eyeball_center_right_y,
-            self.eyeball_center_right_z,
-        ])
+        return self._data[:, [8, 9, 10]]
 
     @property
     def optical_axis_left(self) -> npt.NDArray[np.float64]:
-        return np.column_stack([
-            self.optical_axis_left_x,
-            self.optical_axis_left_y,
-            self.optical_axis_left_z,
-        ])
+        return self._data[:, [4, 5, 6]]
 
     @property
     def optical_axis_right(self) -> npt.NDArray[np.float64]:
-        return np.column_stack([
-            self.optical_axis_right_x,
-            self.optical_axis_right_y,
-            self.optical_axis_right_z,
-        ])
+        return self._data[:, [11, 12, 13]]
 
-    def __getattr__(self, key: str) -> npt.NDArray:
-        return self[key]
+    @property
+    def data(self) -> npt.NDArray[np.float64]:
+        return self._data
+
+    def __getitem__(self, key: int) -> EyeStateRecord:
+        record = EyeStateRecord(self._time_data[key], *self._data[key])
+        return record
