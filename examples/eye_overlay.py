a10034618f97ca20e087bb70464605fbf5fb52ce
Marc Tonsen <mar@pupil-labs.com>
2024-11-10 13:58:24 +0100

    Update gaze_overlay and move grayframe to shared utils file

--- examples/eye_overlay.py
+++ examples/eye_overlay.py
@@ -3,33 +3,11 @@ import sys
 import cv2
 import numpy as np
 from tqdm import tqdm
+from utils import GrayFrame
 
 import pupil_labs.neon_recording as nr
 from pupil_labs.matching import Matcher
 from pupil_labs.video import Writer
-
-
-class GrayFrame:
-    def __init__(self, width, height):
-        self.width = width
-        self.height = height
-
-        self._bgr = None
-        self._gray = None
-
-    @property
-    def bgr(self):
-        if self._bgr is None:
-            self._bgr = 128 * np.ones([self.height, self.width, 3], dtype="uint8")
-
-        return self._bgr
-
-    @property
-    def gray(self):
-        if self._gray is None:
-            self._gray = 128 * np.ones([self.height, self.width], dtype="uint8")
-
-        return self._gray
 
 
 def overlay_image(img, img_overlay, x, y):
