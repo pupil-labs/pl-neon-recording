f5db4b40b89d1e35f07581bdb0f44333387c4bc8
Marc Tonsen <mar@pupil-labs.com>
2024-11-15 17:04:47 +0100

    Added GrayFrame to utils

--- src/pupil_labs/neon_recording/utils.py
+++ src/pupil_labs/neon_recording/utils.py
@@ -55,3 +55,26 @@ def load_multipart_timestamps(files, concatenate=True):
         timestamps = np.concatenate(timestamps)
 
     return timestamps
+
+
+class GrayFrame:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+        self._bgr = None
+        self._gray = None
+
+    @property
+    def bgr(self):
+        if self._bgr is None:
+            self._bgr = 128 * np.ones([self.height, self.width, 3], dtype="uint8")
+
+        return self._bgr
+
+    @property
+    def gray(self):
+        if self._gray is None:
+            self._gray = 128 * np.ones([self.height, self.width], dtype="uint8")
+
+        return self._gray
