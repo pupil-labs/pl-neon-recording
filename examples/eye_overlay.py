d2c2ad13a56c7b5ee0804574f2ef60a4dbafe0ed
Marc Tonsen <mar@pupil-labs.com>
2024-11-15 17:04:47 +0100

    Added GrayFrame to utils

--- examples/eye_overlay.py
+++ examples/eye_overlay.py
@@ -3,10 +3,10 @@ import sys
 import cv2
 import numpy as np
 from tqdm import tqdm
-from utils import GrayFrame
 
 import pupil_labs.neon_recording as nr
-from pupil_labs.matching import Matcher
+from pupil_labs.matching import MatchedIndividual
+from pupil_labs.neon_recording.utils import GrayFrame
 from pupil_labs.video import Writer
 
 
@@ -34,8 +34,8 @@ def make_overlaid_video(recording_dir, output_video_path, fps=30):
         recording.eye.timestamps[0], recording.scene.timestamps[-1], 1 / fps
     )
     matched_data = zip(
-        Matcher(target_timestamps, recording.scene, tolerance=2 / fps),
-        Matcher(target_timestamps, recording.eye, tolerance=2 / fps),
+        MatchedIndividual(target_timestamps, recording.scene, tolerance=2 / fps),
+        MatchedIndividual(target_timestamps, recording.eye, tolerance=2 / fps),
     )
 
     with Writer(output_video_path) as video_writer:
