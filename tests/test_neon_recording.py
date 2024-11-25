da4abbd2e7a191e551a05fd6367ab8f25a06eafb
Marc Tonsen <mar@pupil-labs.com>
2024-11-25 14:30:00 +0100

    Updated test

--- tests/test_neon_recording.py
+++ tests/test_neon_recording.py
@@ -16,12 +16,10 @@ def test_rec_info(rec: nr.NeonRecording):
     assert rec.device_serial == "537468"
     assert rec.wearer["name"] == "Fon "
     assert rec.wearer["uuid"] == "ade59fa1-67d7-4750-86c0-29c1deb28f80"
-    assert rec.start_ts_ns == 1726825501543000000
-    assert rec.start_ts == 1726825501.5430002
-    assert rec.start_ts_ns * 1e-9 == rec.start_ts
+    assert rec.start_ts == 1726825501543000000
     assert rec.duration_ns == 11013000000
-    assert rec.duration == 11.013
-    assert rec.duration_ns * 1e-9 == rec.duration
+    assert rec.duration_sec == 11.013
+    assert rec.duration_ns * 1e-9 == rec.duration_sec
 
 
 def test_calibration(rec: nr.NeonRecording):
