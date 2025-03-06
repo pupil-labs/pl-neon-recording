8f03a049d44d006e8f3f31b7cc463679d65dfc07
Daniel Dourvaris <dourvaris@gmail.com>
2025-03-06 16:54:03 +0200

    Fix gaze_overlay example

--- examples/gaze_overlay.py
+++ examples/gaze_overlay.py
@@ -2,10 +2,10 @@ import sys
 
 import cv2
 import numpy as np
+from tqdm import tqdm
+
 import pupil_labs.neon_recording as nr
 from pupil_labs.neon_recording.stream.av_stream.video_stream import GrayFrame
-
-from tqdm import tqdm
 
 
 def make_overlaid_video(recording_dir, output_video_path, fps=None):
@@ -13,15 +13,17 @@ def make_overlaid_video(recording_dir, output_video_path, fps=None):
 
     video_writer = cv2.VideoWriter(
         str(output_video_path),
-        cv2.VideoWriter_fourcc(*'MJPG'),
+        cv2.VideoWriter_fourcc(*"MJPG"),
         fps,
-        (recording.scene.width, recording.scene.height)
+        (recording.scene.width, recording.scene.height),
     )
 
     if fps is None:
         output_timestamps = recording.scene.ts
     else:
-        output_timestamps = np.arange(recording.scene.ts[0], recording.scene.ts[-1], 1 / fps)
+        output_timestamps = np.arange(
+            recording.scene.ts[0], recording.scene.ts[-1], 1e9 / fps
+        )
 
     scene_datas = recording.scene.sample(output_timestamps)
     combined_data = zip(
@@ -30,27 +32,29 @@ def make_overlaid_video(recording_dir, output_video_path, fps=None):
         recording.gaze.sample(output_timestamps),
     )
 
-    for ts, scene_frame, gaze_datum in tqdm(combined_data, total=len(output_timestamps)):
-        if abs(scene_frame.ts - ts) < 2 / fps:
+    for ts, scene_frame, gaze_datum in tqdm(
+        combined_data, total=len(output_timestamps)
+    ):
+        if abs(scene_frame.ts - ts) < 2e9 / fps:
             frame_pixels = scene_frame.bgr
         else:
             frame_pixels = GrayFrame(scene_frame.width, scene_frame.height).bgr
 
-        if abs(gaze_datum.ts - ts) < 2 / fps:
+        if abs(gaze_datum.ts - ts) < 2e9 / fps:
             frame_pixels = cv2.circle(
                 frame_pixels,
                 (int(gaze_datum.x), int(gaze_datum.y)),
                 50,
                 (0, 0, 255),
-                10
+                10,
             )
 
         video_writer.write(frame_pixels)
-        cv2.imshow('Frame', frame_pixels)
+        cv2.imshow("Frame", frame_pixels)
         cv2.pollKey()
 
     video_writer.release()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     make_overlaid_video(sys.argv[1], "gaze-overlay-output-video.avi", 24)
