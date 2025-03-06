d947d2c33ed1c85dabe3b32338a5b6ad1507a89c
Daniel Dourvaris <dourvaris@gmail.com>
2025-03-06 13:25:05 +0200

    Update find_clap example

--- examples/find_clap.py
+++ examples/find_clap.py
@@ -1,9 +1,8 @@
 import sys
 
 import numpy as np
-import pupil_labs.neon_recording as nr
 
-from tqdm import tqdm
+import pupil_labs.neon_recording as nr
 
 
 def find_clap(recording_dir):
@@ -14,15 +13,17 @@ def find_clap(recording_dir):
 
     for frame in recording.audio:
         audio_data = frame.to_ndarray()
-        rms = np.sqrt(np.mean(audio_data ** 2))
+        rms = np.sqrt(np.mean(audio_data**2))
 
         if rms > max_rms:
             max_rms = rms
             max_time = frame.ts
 
     rel_time = max_time - recording.start_ts
-    print(F"The loudest audio occurs at {max_time} (rel={rel_time}) with an rms = {max_rms}.")
+    print(
+        f"The loudest audio occurs at {max_time} (rel={rel_time}) with an rms = {max_rms}."
+    )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     find_clap(sys.argv[1])
