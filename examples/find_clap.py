600fbefcab52db20a3375bf0043df9dbcf2dcb41
Dominic Canare <dom@dominiccanare.com>
2025-06-10 03:21:48 -0500

    Fix: use RMS on overlapping windows (instead of discrete frames) when searching for loudest audio

--- examples/find_clap.py
+++ examples/find_clap.py
@@ -1,26 +1,31 @@
 import sys
 
 import numpy as np
+from tqdm import tqdm
 
 import pupil_labs.neon_recording as nr
 
 
-def find_clap(recording_dir):
+def find_clap(recording_dir, window_size_seconds=0.1):
     recording = nr.open(recording_dir)
+    sample_count = int(window_size_seconds * recording.audio.rate)
 
-    max_rms = -1
-    max_time = -1
+    audio_frames = [frame.to_ndarray() for frame in recording.audio]
+    audio_data = np.concatenate(audio_frames).flatten()
 
-    for frame in recording.audio:
-        audio_data = frame.to_ndarray()
-        rms = np.sqrt(np.mean(audio_data**2))
+    max_rms = 0
+    loudest_time = 0
+
+    for i in tqdm(range(len(audio_data) - sample_count)):
+        segment = audio_data[i:i + sample_count]
+        rms = np.sqrt(np.mean(np.square(segment)))
 
         if rms > max_rms:
             max_rms = rms
-            max_time = frame.ts
+            loudest_time = float(i) / recording.audio.rate
 
-    rel_time = (max_time - recording.start_ts) / 1e9
-    print(f"The loudest audio occurs at {max_time} (rel={rel_time}), rms = {max_rms}.")
+    abs_time = loudest_time * 1e9 + recording.start_ts
+    print(f"The loudest audio occurs at {abs_time} (rel={loudest_time}), rms = {max_rms}.")
 
 
 if __name__ == "__main__":
